6b70758ad49361b99cfaa7b3c808e192c5a970b1
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-09-23 12:16:28 +0400

    Finish styling footer top part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,42 +12,44 @@ import footerbg from '../images/footerbg.png';
 
 function Footer() {
   return (
-    <footer className="relative">
-      <div className="w-full h-80 bg-black absolute z-10 opacity-95"></div>
-      <div className="footerbg absolute w-full h-80">
+    <footer className="relative h-96">
+      <div className="w-full bottom-0 h-full bg-black absolute z-10 opacity-95"></div>
+      <div className="absolute bottom-0 w-full h-full">
         <img className="w-full h-full logo" src={footerbg} alt="footerbg" />
       </div>
-      <div className="z-30 absolute w-full h-80 flex flex-col px-4 py-6">
-        <div className="flex justify-around border-2 flex items-center my-12">
-          <div>(919) 382-2001</div>
-          <div>
+      <div className="z-30 bottom-0 absolute w-full flex flex-col px-4 py-6">
+        <div className="flex items-center my-12">
+          <div className="flex-1 text-white text-center text-2xl">(919) 382-2001</div>
+          <div className="flex-1 flex justify-center">
             <Link to="/">
-              <img className="circle w-20 h-20 logo" src={logo} alt="logo" />
+              <img className="circle w-40 h-40" src={logo} alt="img" />
             </Link>
           </div>
-          <ul className="flex">
-            <li>
-              <img src={f} alt="facebook" />
-            </li>
-            <li>
-              <img src={t} alt="twitter" />
-            </li>
-            <li>
-              <img src={i} alt="instagram" />
-            </li>
-            <li>
-              <img src={l} alt="linkedin" />
-            </li>
-            <li>
-              <img src={p} alt="pinterest" />
-            </li>
-          </ul>
+          <div className="flex-1">
+            <ul className='flex justify-between w-3/4 m-auto'>
+              <li>
+                <img className="w-8 h-8" src={f} alt="facebook" />
+              </li>
+              <li>
+                <img className="w-8 h-8" src={t} alt="twitter" />
+              </li>
+              <li>
+                <img className="w-8 h-8" src={i} alt="instagram" />
+              </li>
+              <li>
+                <img className="w-8 h-8" src={l} alt="linkedin" />
+              </li>
+              <li>
+                <img className="w-8 h-8" src={p} alt="pinterest" />
+              </li>
+            </ul>
+          </div>
         </div>
 
         {/* second */}
-        <div className="flex justify-around">
+        <div className="flex justify-around items-center">
           <div className="flex-1">
-            <div className="w-1/3 flex justify-between m-auto">
+            <div className="w-2/5 flex justify-between m-auto">
               <span>
                 <img className="w-10 h-10" src={h} alt="house" />
               </span>
