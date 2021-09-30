15049202a2d02c7cb6a75e309e8ef846bdfce6ed
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-09-30 13:29:43 +0400

    Add responsive carousel

--- src/pages/House.page.js
+++ src/pages/House.page.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
 import re4 from '../images/realEstate/re4.jpg';
 import { fetchHouseById } from '../store/actions';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 function HousePage() {
   const dispatch = useDispatch();
@@ -13,6 +17,8 @@ function HousePage() {
     window.scrollTo(0, 0);
     dispatch(fetchHouseById(`http://localhost:3000/houses/${houseId}`));
   }, [dispatch, houseId]);
+
+  const house = useSelector((state) => state.housesReducer.choosedHouse);
 
   return (
     <>
@@ -26,14 +32,28 @@ function HousePage() {
           backgroundImage: `url(${re4})`,
         }}
       >
-        <div className="text-center z-40 w-full flex flex-col items-center">
-          <h1 className="text-6xl text-white mb-3 font-caramel">Our Agents</h1>
-        </div>
+        {house && (
+          <div className="text-center z-40 w-full flex flex-col items-center">
+            <h1 className="text-6xl text-white w-2/5 mb-3 font-caramel mt-10">
+              {house && house.address}
+            </h1>
+          </div>
+        )}
         <div className="w-full h-full bg-black absolute z-10 opacity-75"></div>
       </section>
-      <section className="p-20 bg-gray-300">
-        d{/* {loading ? <Loader /> : <AgentsList allAgents={agents} />} */}
-      </section>
+      {house ? (
+        <section className="p-20 bg-gray-300 flex justify-center">
+          <Carousel interval={1500} autoPlay infiniteLoop className='w-1/2 -mt-44 z-10'>
+            {house.pictures.map((pic,i) => (
+              <div key={i}>
+                <img src={pic} alt='home_pic'/>
+              </div>
+            ))}
+          </Carousel>
+        </section>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 }
