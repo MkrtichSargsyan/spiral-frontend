f738096899cf5e63bdf29b07c1bc5fe254a372f9
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-10-10 21:24:42 +0400

    Make aboutus section responsive

--- src/components/mainPage_components/AboutUs.js
+++ src/components/mainPage_components/AboutUs.js
@@ -3,10 +3,10 @@ import AnimButton from '../../ui_kits/AnimButton';
 
 function AboutUs() {
   return (
-    <section className="bg-gray-300 flex items-center flex-col py-10 pt-56">
+    <section className="bg-gray-300 flex items-center flex-col pt-56">
       <h2 className="text-4xl text-gray-400 mt-16 mb-2 relative">About Us</h2>
       <hr className="w-10 mb-10" />
-      <p className="w-2/5 text-xl text-center">
+      <p className="w-full px-4 md:px-20 xl:w-2/5 text-xl text-center">
         <span className="text-2xl font-bold italic mr-2">Spiral</span> is a
         full-service Residential, Commercial and Real Estate Management company
         serving Yerevan since 1888. At <span className="italic">Spiral</span>{' '}
