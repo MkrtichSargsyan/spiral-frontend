f9a409822a7daa7ff88f8a784dd461c841645ae2
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2022-04-22 14:03:40 +0400

    Change name

--- src/components/mainPage_components/Showcase.js
+++ src/components/mainPage_components/Showcase.js
@@ -10,7 +10,7 @@ function Showcase() {
           Find your dream home in
         </h1>
         <h2 className="text-4xl italic text-gray-300 mb-6 font-serif">
-          Yerevan
+          London
         </h2>
         <div className="w-1/2 md:px-0 md:w-1/3 h-12 lg:h-16">
           <MyInput />
